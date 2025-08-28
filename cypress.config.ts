96faf377249a5fe7f51f79bee1bfd3268538a114
Peter <peter.warholm@gmail.com>
2025-08-28 11:42:43 +0200

    feat: refactor MongoMemoryReplSet initialization and cleanup process in Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,14 +2,17 @@ import { spawn } from 'child_process';
 import { defineConfig } from 'cypress';
 import { MongoMemoryReplSet } from 'mongodb-memory-server';
 import waitOn from 'wait-on';
+import { db } from './prisma/db';
 import { seedTodos } from './prisma/seed/todo';
 
 export default defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       // 1. Skapa en in-memory databas (replica set prisma gnäller annars)
-      const db = await MongoMemoryReplSet.create({ replSet: { count: 1 } });
-      const dbUri = db.getUri('cypress-test');
+      const mongo = await MongoMemoryReplSet.create({
+        replSet: { count: 1 },
+      });
+      const dbUri = mongo.getUri('cypress-test');
 
       // 2. Starta Next.js servern (på en annan port ex. 3100, som ansluter till 1)
       const server = spawn(
@@ -28,6 +31,12 @@ export default defineConfig({
       await waitOn({ resources: ['http://localhost:3100'], timeout: 60_000 });
 
       // 4. Städa upp processerna, det vill säga mongo-databasen och Next.js-servern
+      const cleanup = async () => {
+        server.kill();
+        await mongo.stop();
+      };
+      process.on('exit', cleanup);
+
       // 5. Återså/reseed databasen så att testerna blir oberoende av varandra
 
       on('task', {
