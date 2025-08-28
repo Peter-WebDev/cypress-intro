8a10fbbf7f5e416c27acf7066968cd16ec77ae37
Peter <peter.warholm@gmail.com>
2025-08-28 10:37:47 +0200

    feat: add comments to task setup in Cypress config for better clarity

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', {
+        // 1. Skapa en in-memory databas (replica set prisma gnäller annars)
+        // 2. Starta Next.js servern (på en annan port ex. 3100, som ansluter till 1)
+        // 3. Vänta på att Next.js-servern är igång innan cypress kör vidare
+        // 4. Städa upp processerna, det vill säga mongo-databasen och Next.js-servern
+        // 5. Återså/reseed databasen så att testerna blir oberoende av varandra
+
         async reseed() {
           await db.todo.deleteMany();
           await seedTodos();
