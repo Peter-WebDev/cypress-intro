10b484e6fe8c6b967aa38e572aa9536e0e38c339
Peter <peter.warholm@gmail.com>
2025-08-28 23:21:22 +0200

    Update next config to auto handle writing to next map with test and dev environment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: process.env.NODE_ENV === 'test' ? '.next-test' : '.test',
+  turbopack: {
+    root: path.join(__dirname, '..'),
+  },
 };
 
 export default nextConfig;
