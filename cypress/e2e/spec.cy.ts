628d58b645bc5a3205814f3dfed86cc0d7cb825b
Peter <peter.warholm@gmail.com>
2025-08-28 13:23:07 +0200

    fix: update visit URL in tests to use relative path

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,7 +4,7 @@ describe('todo', () => {
   });
 
   it('should display three todos by default', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.get('li').should('have.length', 3);
     cy.get('li').first().should('contain.text', 'Feed the cat');
     cy.get('li').last().contains('Walk all the cats');
@@ -13,7 +13,7 @@ describe('todo', () => {
 
 describe('delete todo', () => {
   it('should be possible to delete a todo', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.contains('Feed the cat').parents('li').find('button').click();
     cy.get('li').should('have.length', 2);
     cy.contains('Feed the cat').should('not.exist');
@@ -22,7 +22,7 @@ describe('delete todo', () => {
 
 describe('add todo', () => {
   it('should be possible to add a todo', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.get('input[type="text"]').type('My first post');
     cy.get('button').contains('Add todo').click();
     cy.get('li').should('contain', 'My first post');
