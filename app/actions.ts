67e93ef876f5ebebd10ede97f57a57befdb04c1b
Peter <peter.warholm@gmail.com>
2025-08-30 18:13:13 +0200

    Setup zod schema

--- app/actions.ts
+++ app/actions.ts
@@ -2,13 +2,23 @@
 
 import { db } from '@/prisma/db';
 import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
+
+const gameResultSchema = z.object({
+  name: z.string().min(3, {
+    message: 'Name must be at least 3 characters long',
+  }),
+  time: z.coerce.number().int().positive(),
+  attempts: z.coerce.number().int().positive(),
+  categoryId: z.string().min(1),
+});
 
 export async function addTodo(formData: FormData) {
   const text = formData.get('text') as string;
 
   if (!text.trim()) return;
 
-  await db.todo.create({
+  await db.asset.create({
     data: { text: text.trim() },
   });
 
