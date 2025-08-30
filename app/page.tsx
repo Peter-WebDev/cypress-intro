c498a343faed5fa29ff907bb3b4b36d215542ac7
Peter <peter.warholm@gmail.com>
2025-08-30 20:31:33 +0200

    Cleared old page, set new h1

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import { db } from "@/prisma/db";
-import { TodoForm } from "./ui/todo-form";
-import TodoList from "./ui/todo-list";
-
 export default async function Home() {
-  const todos = await db.todo.findMany();
-
   return (
     <main>
-      <h1 className="text-2xl font-bold mb-6">Todo App</h1>
-      <TodoList todos={todos} />
-      <TodoForm />
+      <h1 className="text-2xl font-bold mb-6">Memory</h1>
     </main >
   );
 }
