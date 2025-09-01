e53e72b890a36d24c06813696f25657f1929e29f
Peter <peter.warholm@gmail.com>
2025-09-01 20:48:41 +0200

    Update next config to handle remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.join(__dirname, '..'),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
